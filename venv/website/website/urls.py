6cd646aa53a280a51c15267b7f4e8fc55fada88e
Akash Kalpattu <srinivasan.kalpattu@student.adelaide.edu.au>
2023-08-08 11:42:46 +0930

    Updated first app, redirect from main page to first_app

--- venv/website/website/urls.py
+++ venv/website/website/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('first_app/', include('first_app.urls')),
+    path('', RedirectView.as_view(url='first_app/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

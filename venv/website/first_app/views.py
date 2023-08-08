13812afc8d5b105e81d4fdf14448deaa896c5607
Akash Kalpattu <srinivasan.kalpattu@student.adelaide.edu.au>
2023-08-08 12:21:13 +0930

    Created basic html and css for initial testing

--- venv/website/first_app/views.py
+++ venv/website/first_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Email_Model
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the main page.")
+    email_count = Email_Model.objects.count()
+
+    context = {
+        'email_count': email_count
+    }
+
+    return render(request, 'index.html', context=context)

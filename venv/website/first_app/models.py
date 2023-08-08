b3726ea3e88cb641bece5bbd83138c51b8bd3b89
Akash Kalpattu <srinivasan.kalpattu@student.adelaide.edu.au>
2023-08-08 12:08:15 +0930

    Added models and admin user

--- venv/website/first_app/models.py
+++ venv/website/first_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Email_Model(models.Model):
+    email = models.EmailField(max_length=30, help_text="Enter your email address")
+
+    class Meta:
+        ordering = ['email']
+
+    def __str__(self):
+        return self.email
+
+    def get_absolute_url(self):
+        """Returns the URL to access a particular instance of the model."""
+        return reverse('model-detail-view', args=[str(self.id)])
+
